5cbca60d6b4da177a468c4e4a01eba58ff102205
Colin Brineman <colinbrineman@Colins-MacBook-Pro.local>
2022-01-12 18:32:37 -0500

    7.2.4

--- schema.sql
+++ schema.sql
@@ -1,19 +1,31 @@
--- Creating tables for PH-EmployeeDB
+-- Creating tables for PH-EmployeeDB; p-keys deleted to override import error due to duplicate 'emp_no' for 'titles' (5-6) and 'debt_emp' (11-12)
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
 CREATE TABLE employees (
      emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
+     first_name VARCHAR(14) NOT NULL,
+     last_name VARCHAR(16) NOT NULL,
+     gender VARCHAR(1) NOT NULL,
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE salaries (
+  	emp_no INT NOT NULL,
+  	salary INT NOT NULL,
+  	from_date DATE NOT NULL,
+  	to_date DATE NOT NULL,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no)
+);
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -23,30 +35,20 @@ CREATE TABLE dept_manager (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
-CREATE TABLE salaries (
-  	emp_no INT NOT NULL,
-  	salary INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
-);
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR(18) NOT NULL,
 	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-CREATE TABLE debt_emp (
-	dept_no VARCHAR(4) NOT NULL,
+
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-)
-
-SELECT * FROM departments;
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
